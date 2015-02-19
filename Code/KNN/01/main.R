944244cecd61eec84cc420c0ace6df310779af6f
Croomch <lschw12@student.sdu.dk>
2015-02-19 12:52:54 +0100

    Updated version, k can now be a vector

--- Code/KNN/01/main.R
+++ Code/KNN/01/main.R
@@ -7,12 +7,12 @@ library("class")
 library("gmodels")
 
 # jsut in time... speed up
-require(compiler)
+# require(compiler)
 # 0 - turn off JIT
 # 1 - compile closures before they are called the first time
 # 2 - same as 1, plus compile closures before duplicating (useful for packages that store closures in lists, like lattice)
 # 3 - same as 2, plus compile all for(), while(), and repeat() loops before executing.
-enableJIT(3) # does this even work!?!?!...
+# enableJIT(3) # does this even work!?!?!...
 
 #-------------------------------------------------------------
 #Functions to be used in evaluation
@@ -24,75 +24,75 @@ source("given_functions.R")
 # generates a set of dataelements where: [[set]][training/testing (1/2)][number ('0','1',..)][trial, pixel]
 # input data to split in format [[number ('0','1',..)][trial, pixel], split ratio = percentage of set being in training set, sets = number of different sets.
 getSystematicSplit <- function(data, splitRatio, sets) { 
-  noTrials = dim(data[[1]])[1] # number of ciphers in the dataset gathered (400 ciphers)
-  sizeOfCipher = dim(data[[1]])[2]
-  
-  print(paste(c("Trials: ",noTrials),collapse=""))
-  print(paste(c("Cipher: ",sizeOfCipher),collapse=""))
-  
-  forDataSet = round(noTrials*splitRatio) # number of ciphers for the training part
-  forTestSet = (noTrials - forDataSet)
- 
-  result1 <- array(,c(sets,10,forDataSet,sizeOfCipher))
-  result2 <- array(,c(sets,10,forTestSet,sizeOfCipher))
-  
-  print(paste(c("train / test: ", forDataSet, " / ", forTestSet),collapse=""))
-  
-  setSize = round(noTrials/sets)
-  
-  for(set in 1:sets)
-  {
-    training = array(,c(10,forDataSet,sizeOfCipher))
-    testing = array(,c(10,forTestSet,sizeOfCipher))
-    
-    BP_start = ((sets-set)*setSize) #break points in data set
-    BP_end = BP_start + forTestSet
-    
-    BP_foldover = BP_end - noTrials
-      
-    for(cipher in 1:10)
-    {
-      if(BP_foldover > 0)
-      {
-        testing[cipher,1:BP_foldover,] <- data[[cipher]][1:BP_foldover,]
-        training[cipher,1:forDataSet,] <- data[[cipher]][(BP_foldover+1):BP_start,]
-        if((BP_foldover) < forTestSet)
-        {
-          testing[cipher,(BP_foldover+1):forTestSet,] <- data[[cipher]][(BP_start+1):noTrials,]
-        }
-      }
-      else
-      {
-        training[cipher,1:BP_start,] <- data[[cipher]][1:BP_start,]
-        testing[cipher,1:forTestSet,] <- data[[cipher]][(BP_start+1):BP_end,]
-        if((BP_start) < forDataSet)
-        {
-          training[cipher,(BP_start+1):(forDataSet),] <- data[[cipher]][(BP_end+1):(noTrials),]
-        }
-      }
-    }
-    result1[set,,,] <- training[]
-    result2[set,,,] <- testing[]
-    
-  }
-  return(list(t1=result1,t2=result2))
+	noTrials = dim(data[[1]])[1] # number of ciphers in the dataset gathered (400 ciphers)
+	sizeOfCipher = dim(data[[1]])[2]
+	
+	print(paste(c("Trials: ",noTrials),collapse=""))
+	print(paste(c("Cipher: ",sizeOfCipher),collapse=""))
+	
+	forDataSet = round(noTrials*splitRatio) # number of ciphers for the training part
+	forTestSet = (noTrials - forDataSet)
+	
+	result1 <- array(,c(sets,10,forDataSet,sizeOfCipher))
+	result2 <- array(,c(sets,10,forTestSet,sizeOfCipher))
+	
+	print(paste(c("train / test: ", forDataSet, " / ", forTestSet),collapse=""))
+	
+	setSize = round(noTrials/sets)
+	
+	for(set in 1:sets)
+	{
+		training = array(,c(10,forDataSet,sizeOfCipher))
+		testing = array(,c(10,forTestSet,sizeOfCipher))
+		
+		BP_start = ((sets-set)*setSize) #break points in data set
+		BP_end = BP_start + forTestSet
+		
+		BP_foldover = BP_end - noTrials
+		
+		for(cipher in 1:10)
+		{
+			if(BP_foldover > 0)
+			{
+				testing[cipher,1:BP_foldover,] <- data[[cipher]][1:BP_foldover,]
+				training[cipher,1:forDataSet,] <- data[[cipher]][(BP_foldover+1):BP_start,]
+				if((BP_foldover) < forTestSet)
+				{
+					testing[cipher,(BP_foldover+1):forTestSet,] <- data[[cipher]][(BP_start+1):noTrials,]
+				}
+			}
+			else
+			{
+				training[cipher,1:BP_start,] <- data[[cipher]][1:BP_start,]
+				testing[cipher,1:forTestSet,] <- data[[cipher]][(BP_start+1):BP_end,]
+				if((BP_start) < forDataSet)
+				{
+					training[cipher,(BP_start+1):(forDataSet),] <- data[[cipher]][(BP_end+1):(noTrials),]
+				}
+			}
+		}
+		result1[set,,,] <- training[]
+		result2[set,,,] <- testing[]
+		
+	}
+	return(list(t1=result1,t2=result2))
 }
 
 KNN_test_one_person <- function(d, s, g, m, k){
 	DPI = c(100,200,300)
 	
 	# trainingDigit (RawTrainData) is filled with the ciphers [[digit eg '0','1',...]][pixel || row, column] one row = one string of the pixel being the letter
-	RawTrainData1 = loadSinglePersonsData(DPI[[1]],g,m);
+	RawTrainData1 = loadSinglePersonsData(DPI[[d]],g,m);
 	
 	# print("data aquired.")
-
+	
 	# split data
 	# print("Spliting dataset...")
-	splitList = getSystematicSplit(RawTrainData1,s,1)
+	splitList = getSystematicSplit(RawTrainData1,s,2)
 	trained <- splitList$t1
 	tested <- splitList$t2
 	# print("Dataset split.")
-
+	
 	timing = proc.time()
 	
 	#run program
@@ -100,108 +100,122 @@ KNN_test_one_person <- function(d, s, g, m, k){
 	
 	timing = proc.time() - timing;
 	timing = timing[["elapsed"]]
-	new_data = append(timing,new_data,after=length(timing))
+	print(paste(c("Time: ",timing),collapse=""))
+	# 	new_data = append(timing,new_data,after=length(timing))
 	
 	#write down data
-# 	data_file = read.csv(file=paste(c("result_G",g,"M",m,".csv"),collapse=""))
-	name = paste(c("K",k,"_S",s,"_D",DPI[d]),collapse="")
-# 	data_file$name = new_data;
-
-	print(new_data)
-# 	write.csv(data_file, file=paste(c("result_G",g,"M",m,".csv"),collapse=""),row.names=FALSE)
+	# 	data_file = read.csv(file=paste(c("result_G",g,"M",m,".csv"),collapse=""))
+	# 	name = paste(c("K",k,"_S",s,"_D",DPI[d]),collapse="")
+	# 	data_file$name = new_data;
+	
+	# 	print(new_data)
+	# 	write.csv(data_file, file=paste(c("result_G",g,"M",m,".csv"),collapse=""),row.names=FALSE)
 }
 
 # calculates the char it is expected to be using KNN
 # test vector is a single vector that represents the chracter
 # trainVectors are the vectors [char type (1-10),element (the train chars stored),pixels]
 # k is the number of neighbours taken into account, is a vector
-KNN <- function(testVector, trainVectors,k){ 
-  noOfChars <- dim(trainVectors)[1]
-  noOfElements <- dim(trainVectors)[2]
-  noOfDimensions <- dim(trainVectors)[3]
-  noK = size(k)
-  maxK = max(k)
-  print(paste(c("size of k: ",noK, " max k: ",maxK),collapse=""))
-  
-  voteing <- array(0,c(noOfChars)) # used for calculating votes using bucket sort
-  neighbours <- array(Inf,c(maxK,2)) # used to store: (dist,char) for knn, default = Infinity
-  testDistance <- array(,c(2,noOfDimensions))
-  testDistance[1,] <- testVector[] # laod the testvector into the matrix used to compute distance
-  
-  for(char in 1:noOfChars){
-    for(element in 1:noOfElements){
-      # for each element in each char compute the distance
-      testDistance[2,] <- trainVectors[char,element,]
-      distance <- dist(testDistance,method = "euclidean")
-      # if the distance is less than those already stored, add it (dist, char)
-      if(neighbours[maxK,1] > distance){
-        neighbours[maxK,] <- c(distance,char)
-        i <- (maxK-1)
-        while(neighbours[i,1] > distance && i > 0){
-          neighbours[i+1,] <- neighbours[i,]
-          neighbours[i,] <- c(distance,char)
-          i <- (i-1)
-        }
-      }
-    }
-  }
-  
-  # determine the result (most voted) (bucket sort)
-  kreturne <- 1:noK
-  for(kval in 1:noK){
-  	voteing() = 0 # clear bucket sort array
-  	for(sort in 1:k(kval)){
-  		elementVotedFor <- neighbours[sort,2] # (values from 1 to noOfChars)
-  		if(elementVotedFor != Inf){
-  			voteing[elementVotedFor] <- (voteing[elementVotedFor] + 1)
-  		}
-  	}
-  	
-  	# find the most voted
-  	result <- 1
-  	for(j in 2:noOfChars){
-  		if(voteing[result] < voteing[j]){
-  			result <-j
-  		}
-  	}
-  	
-  	kreturne(kval) <- result
-  }
-  
-  return(kreturn)
+KNN <- function(testVector, trainVectors, k){ 
+	noOfChars <- dim(trainVectors)[1]
+	noOfElements <- dim(trainVectors)[2]
+	noOfDimensions <- dim(trainVectors)[3]
+	noK <- length(k)
+	maxK <- max(k)
+	#   print(c("size of k: ",noK, " max k: ",maxK))
+	
+	neighbours <- array(Inf,c(maxK,2)) # used to store: (dist,char) for knn, default = Infinity
+	testDistance <- array(,c(2,noOfDimensions))
+	testDistance[1,] <- testVector[] # laod the testvector into the matrix used to compute distance
+	
+	for(char in 1:noOfChars){
+		for(element in 1:noOfElements){
+			# for each element in each char compute the distance
+			testDistance[2,] <- trainVectors[char,element,]
+			distance <- dist(testDistance,method = "euclidean")
+			# if the distance is less than those already stored, add it (dist, char)
+			if(neighbours[maxK,1] > distance){
+				neighbours[maxK,] <- c(distance,char)
+				i <- (maxK-1)
+				while(neighbours[i,1] > distance && i > 0){
+					neighbours[i+1,] <- neighbours[i,]
+					neighbours[i,] <- c(distance,char)
+					i <- (i-1)
+				}
+			}
+		}
+	}
+	
+	# determine the result (most voted) (bucket sort)
+	kreturne <- 1:noK
+	for(kval in 1:noK){
+		voteing <- array(0,c(noOfChars)) # used for calculating votes using bucket sort
+		for(sort in 1:k[kval]){
+			elementVotedFor <- neighbours[sort,2] # (values from 1 to noOfChars)
+			if(elementVotedFor != Inf){
+				voteing[elementVotedFor] <- (voteing[elementVotedFor] + 1)
+			}
+		}
+		
+		# find the most voted
+		result <- 1
+		for(j in 2:noOfChars){
+			if(voteing[result] < voteing[j]){
+				result <-j
+			}
+		}
+		
+		kreturne[kval] <- result
+	}
+	
+	return(kreturne)
 }
 
 percentageDetected   <- function(testData, trainData,k){
 	testSets = dim(testData)[1];
 	testChars = dim(testData)[2];
 	testElements = dim(testData)[3];
+	noK = length(k)
+	# 	print(c("size of k: ",noK))
 	
-	percentageVec = array(,testSets);
-
-	confus = array(0,c(10,10))
+	percentageVec = array(,c(noK));
+	
+	confus = array(0,c(noK,testChars,testChars)) # k, char, cunfused.
 	for(testSet in 1:testSets){
-		
-		trueDetections = array(0,c())
-
 		# do for each testset
 		for(char in 1:testChars){
 			for(element in 1:testElements){
-				result = KNN(testData[testSet,char,element,],trainData[testSet,,,],k) #number of neighbours
+				result = KNN(testData[testSet,char,element,],trainData[testSet,,,], k) #number of neighbours
 				# a vector is returned...
-				if(result == char){
-					trueDetections = (trueDetections + 1)
+				# 				if(result == char){ # does not work with k a vector
+				# 					trueDetections = (trueDetections + 1)
+				# 				}
+				# 				confus[result,char] = confus[result,char]+1
+				for(kRes in 1:noK){
+					confus[kRes,result[kRes],char] = confus[kRes,result[kRes],char] + 1
 				}
-				confus[result,char] = confus[result,char]+1
 			}
 		}
-		percentageVec[[testSet]] = trueDetections/(testChars*testElements)
 	}
-	print(confus)
-	colnames(confus) = 1:10
-	rownames(confus) = 1:10
-	colnames(confus)[1] = "&1" 
-	rownames(confus)[1] = "\hline\n1" 
-	write.table(confus, file="test_file.tex", append = FALSE, sep="\t& ", eol=" \\\\\n", col.names = TRUE, row.names = TRUE, quote = FALSE)
+	for(perK in 1:noK){ # count true detections (the diagonal) and find percentage
+		trueDetections = 0
+		for(i in 1:testChars){
+			trueDetections <- (trueDetections + confus[perK,i,i])
+		}
+		percentageVec[perK] = (trueDetections/(testChars*testElements*testSets))
+	}
+# 	print("confus 1: ")
+# 	print(confus[1,,])
+
+# 	print(c("confus 3: ",confus[3,,]))
+	# 	colnames(confus) = 1:10
+	# 	rownames(confus) = 1:10
+	# 	colnames(confus)[1] = "&1" 
+	# 	rownames(confus)[1] = "\\hline\n1" 
+	# 	write.table(confus, file="test_file.tex", append = FALSE, sep="\t& ", eol=" \\\\\n", col.names = TRUE, row.names = TRUE, quote = FALSE)
+# 	print("vector print:")
+# 	print(percentageVec)
+
 	return(percentageVec);
 }
 
@@ -250,6 +264,53 @@ use_input <- function() {
 	}
 }
 
+# generates data for a contour plot
+# k and train are vectors with the vlaues wanted to test
+getContours <- function(data, kVlues, trainValues){ 
+	sizeK = length(k)
+	sizeT = length(train)
+	
+	resultContour <- array(0,c(sizeT,sizeK))
+	
+	for(runs in 1:sizeT){
+		
+		
+	}
+	
+	DPI = c(100,200,300)
+	
+	# trainingDigit (RawTrainData) is filled with the ciphers [[digit eg '0','1',...]][pixel || row, column] one row = one string of the pixel being the letter
+	RawTrainData1 = loadSinglePersonsData(DPI[[d]],g,m);
+	
+	# print("data aquired.")
+	
+	# split data
+	# print("Spliting dataset...")
+	splitList = getSystematicSplit(RawTrainData1,s,2)
+	trained <- splitList$t1
+	tested <- splitList$t2
+	# print("Dataset split.")
+	
+	timing = proc.time()
+	
+	#run program
+	new_data = percentageDetected(tested,trained,k);
+	
+	timing = proc.time() - timing;
+	timing = timing[["elapsed"]]
+	print(paste(c("Time: ",timing),collapse=""))
+	# 	new_data = append(timing,new_data,after=length(timing))
+	
+	#write down data
+	# 	data_file = read.csv(file=paste(c("result_G",g,"M",m,".csv"),collapse=""))
+	# 	name = paste(c("K",k,"_S",s,"_D",DPI[d]),collapse="")
+	# 	data_file$name = new_data;
+	
+	# 	print(new_data)
+	# 	write.csv(data_file, file=paste(c("result_G",g,"M",m,".csv"),collapse=""),row.names=FALSE)
+	
+}
+
 #-------------------------------------------------------------
 #This is the "main function" or the code that is actualy run
 #-------------------------------------------------------------
@@ -257,7 +318,8 @@ use_input <- function() {
 
 
 #                   dpi split group member k
-KNN_test_one_person(1, 0.5, 3, 2, 1)
+ktest = c(1,5,10)
+
+KNN_test_one_person(1, 0.9, 3, 2, ktest)
 # writes to csv
 #use_input();
-
