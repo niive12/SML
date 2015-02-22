4871f7dfabdeea3d40a8e53996215c369598194b
niive12 <niive12@student.sdu.dk>
2015-02-22 11:39:17 +0000

    updated graphs. Added picture to front page...

--- Code/KNN/01/cross_test.R
+++ Code/KNN/01/cross_test.R
@@ -1,3 +1,4 @@
+
 source("main.R") 
 ktest = 10;
 dpi = c(100,200,300)
@@ -8,17 +9,17 @@ split2 = 0.5
 #each test is the same so variance will always be 0
 no_of_tests = 10;
 
-mean = data.frame(G3M2_9=c(0,0,0)); 
- var = data.frame(G3M2_9=c(0,0,0));
+mean = data.frame(G3M1_9=c(0,0,0)); 
+ var = data.frame(G3M1_9=c(0,0,0));
 dpi_runs = 3
-if( dpi_runs > 0) {
+if( FALSE) {
 	for(i in 1:dpi_runs){
-		data = KNN_test_one_person(i, split2, g, 1, ktest, no_of_tests)
+		data = KNN_test_one_person(i, split1, g, 1, ktest, no_of_tests)
 		mean$G3M1_9[i] = data$mean;
 		 var$G3M1_9[i] = data$var;
 	}
 	for(i in 1:dpi_runs){
-		data = KNN_test_one_person(i, split2, g, 1, ktest, no_of_tests)
+		data = KNN_test_one_person(i, split2, g, 1, ktest, 1)
 		mean$G3M1_5[i] = data$mean
 		 var$G3M1_5[i] = data$var;
 	}                           
@@ -28,44 +29,37 @@ if( dpi_runs > 0) {
 		 var$G3M2_9[i] = data$var;
 	}
 	for(i in 1:dpi_runs){
-		data = KNN_test_one_person(i, split1, g, 2, ktest, no_of_tests)
+		data = KNN_test_one_person(i, split2, g, 2, ktest, 1)
 		mean$G3M2_5[i] = data$mean
 		 var$G3M2_5[i] = data$var;
 	}                           
 } else {
-	mean$G3M2_9= c(0.82975,0.86350,0.89250)
-	mean$G3M2_9= c(0.82975,0.86350,0.89250)
-	mean$G3M2_5= c(0.86000,0.89500,0.92000) 
-	mean$G3M2_5= c(0.85450,0.89975,0.91900)
-	mean$G3M1_9= c(0.77500,0.86025,0.88425)
-	mean$G3M1_9= c(0.77500,0.86025,0.88425)
-	mean$G3M1_5= c(0.77500,0.86025,0.88425)
-	mean$G3M1_5= c(0.77500,0.86025,0.88425)
+	mean$G3M1_9= c(0.68575,0.65525,0.62050) #10 times
+	mean$G3M1_5= c(0.35700,0.35900,0.27350) #1 time
+	mean$G3M2_9= c(0.82975,0.86350,0.89250) #10 times
+	mean$G3M2_5= c(0.79350,0.79350,0.81050) #1 time
 	
-	var$G3M2_9= c(0.82975,0.86350,0.89250)
-	var$G3M2_9= c(0.82975,0.86350,0.89250)
-	var$G3M2_5= c(0.86000,0.89500,0.92000) 
-	var$G3M2_5= c(0.85450,0.89975,0.91900)
-	var$G3M1_9= c(0.77500,0.86025,0.88425)
-	var$G3M1_9= c(0.77500,0.86025,0.88425)
-	var$G3M1_5= c(0.77500,0.86025,0.88425)
-	var$G3M1_5= c(0.77500,0.86025,0.88425)
+	var$G3M1_9= c(0.0260750694444444,0.0274561805555556,0.0398775)
+	var$G3M1_5= c(0.02529,0.86025,0.0708447222222222)
+	var$G3M2_9= c(0.0119325694444444,0.0110044444444444,0.00551527777777778)
+	var$G3M2_5= c(0.0222947222222222,0.0222947222222222,0.0153525)
 }
 print("Cross test is done")
 print(mean) 
-print(var)
+# var = var*10
+print(format(var,digits=3))
 colors = rainbow(4)
-cn = c("G3M1 90/10","G3M1 50/50","G3M2 90/10","G3M2 50/50")
+cn = c("M1 90","M1 50","M2 90","M2 50")
 setEPS()
 postscript("../../../Report/graphics/cross_test.eps",height = 4, width = 8) 
 for(d in 1:4){
 	if(d == 1) {
-		plot(dpi,mean[[d]],type="b",xlab="DPI",ylab="mean success rate",ylim=(c(0.5,1)),col=colors[d])
+		plot(dpi,mean[[d]],type="b",xlab="DPI",ylab="mean success rate",ylim=(c(0.25,0.9)),col=colors[d])
 	} else {
 		lines(dpi,mean[[d]],type="b",lty=d, col=colors[d])
 	}
 }
-legend(100,1,cn,cex=0.8,col=colors,lty=1:5)
+legend(270,0.6,cn,cex=0.8,col=colors,lty=1:4)
 q = dev.off()
 write.latex(format(mean,digits=4), cn, dpi, "../../../Report/graphics/cross_mean.tex")
-write.latex(format(mean,digits=2), cn, dpi, "../../../Report/graphics/cross_var.tex")
+write.latex(format(var,digits=3), cn, dpi, "../../../Report/graphics/cross_var.tex")
--- Code/KNN/01/smoothing.R
+++ Code/KNN/01/smoothing.R
@@ -1,4 +1,4 @@
-# 765m25.808s run time
+# 11h35m43.098s run time
 source("main.R") 
 ktest = 10;
 dpi = c(100,200,300)
@@ -9,9 +9,9 @@ split = 0.9
 no_of_tests = 10;
 
 mean = data.frame(raw=c(0,0,0)); 
-var = data.frame(raw=c(0,0,0));
+var  = data.frame(raw=c(0,0,0));
 dpi_runs = 3
-if( dpi_runs > 0) {
+if( FALSE) {
 	for(i in 1:dpi_runs){
 		data = KNN_test_one_person(i, split, g, m, ktest, no_of_tests)
 		mean$raw[i] = data$mean;
@@ -45,14 +45,15 @@ if( dpi_runs > 0) {
 	mean$ga2 =  c(0.77500,0.86025,0.88425)
 	
 	#haven't meassured yet
-	var$raw =  c(0.82975,0.86350,0.89250)
-	var$avg =  c(0.82975,0.86350,0.89250)
-	var$gap5 = c(0.86000,0.89500,0.92000)     #only 1 test...
-	var$ga1 =  c(0.85450,0.89975,0.91900)
-	var$ga2 =  c(0.77500,0.86025,0.88425)
+	var$raw =  c(0.0119325694444444,0.0110044444444444,0.00551527777777778)
+	var$avg =  c(0.0119325694444444,0.0110044444444444,0.00551527777777778)
+	var$gap5 = c(0.00622256944444444,0.00523861111111111,0.00274090277777778)     #only 1 test...
+	var$ga1 =  c(0.00969833333333333,0.00440618055555556,0.00313361111111111)
+	var$ga2 =  c(0.0179736111111111,0.00795618055555556,0.00587784722222222)
 }
 print(mean) 
-print(var)
+  var = var
+print(format(var,digits=2))
 colors = rainbow(5)
 setEPS()
 postscript("../../../Report/graphics/smoothing.eps",height = 4, width = 8)# 
@@ -67,4 +68,4 @@ legend(261,0.85,c("raw","average","g sigma=0.5","g sigma=1","g sigma=2"),cex=0.8
 q = dev.off()
 cn = c("Raw","Avg","G 0.5","G 1","G 2")
 write.latex(format(mean,digits=4), cn, dpi, "../../../Report/graphics/smoothing_mean.tex")
-write.latex(format(mean,digits=2), cn, dpi, "../../../Report/graphics/smoothing_var.tex")
+write.latex(format(var,digits=2), cn, dpi, "../../../Report/graphics/smoothing_var.tex")
