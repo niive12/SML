fb6c6c4aa0b17ef34aa383d0b1ab7de1ed8492e1
Croomch <lschw12@student.sdu.dk>
2015-02-25 15:42:30 +0100

    Update to toDo and load_people_data

--- Code/KNN/01/load_people_data.R
+++ Code/KNN/01/load_people_data.R
@@ -1,44 +1,10 @@
-source("part01.R")
-get_size <- function(dpi, groupNr, groupMemberNr){
-	corners <- read.csv(paste(c("../../group",groupNr,"/member",groupMemberNr,"/Corners.txt"), collapse = ""))
-  
-	corners = trunc(corners*dpi/300)
-	
-	#extract individual ciffers
-	xStep = (corners[1,7]-corners[1,1])/20;
-	yStep = (corners[1,8]-corners[1,2])/20;
-	size  = (trunc(xStep)-2)*(trunc(yStep)-2)
-	return(size)
-}
+source("given_functions.R")
 
-KNN_test <- function(d, test_people, train_people, s, k){
-	DPI = c(100,200,300)
-	
-	# trainingDigit (RawTrainData) is filled with the ciphers [[digit eg '0','1',...]][pixel || row, column] one row = one string of the pixel being the letter
-	size = get_size(DPI[[d]],test_people[1],test_people[2]);
-	RawTrainData = array(,c(10,((length(test_people)/2)*400),size))
-	for(i in 1:(length(test_people)/2)){
-		print(c("loading group ", test_people[i*2-1],"member ", test_people[i*2]))
-		rawdata = loadSinglePersonsData(DPI[[d]],test_people[i*2-1],test_people[i*2]);
-		RawTrainData[,(1+(400*(i-1))):(400*i),] = rawdata
-	}
-	
-	# print("data aquired.")
-	splitList = getSystematicSplit(RawTrainData,s,1)
-	trained <- splitList$t1
-	tested <- splitList$t2
-	# print("Spliting dataset...")
-	
-	
+# getwd()
+# setwd()
 
-	# split data
-	
-	#run program
-	new_data = percentageDetected(tested,trained,k);
-}
-
-d = 1    # dpi
-s = 0.5 # split tEst
+dpi = 1    # dpi
+split = 0.5 # split tEst
 
 p0 = c(1, 1)
 # p1 = c(1, 2)
