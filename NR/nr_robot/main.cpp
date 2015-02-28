c359eea59cff54cdb8ad4376ffbc2bf18e7e0219
niive12 <niive12@student.sdu.dk>
2015-02-28 12:26:24 +0000

    initial commit

---
+++ NR/nr_robot/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "../NR_LIB/code/nr3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Find expressions for elements\n";
+	cout << "Read files D1, D2, insert in A\n";
+	cout << "Estimate parameters\n";
+	cout << "Estimate resulting errors\n";
+	return 0;
+}
+
