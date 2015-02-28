086349ddb5b1b8ee6caf90c1a30c3838f2bf071d
niive12 <niive12@student.sdu.dk>
2015-02-28 13:18:50 +0000

    dynamically read size of file

--- NR/nr_robot/main.cpp
+++ NR/nr_robot/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <math.h>
 #include "../NR_LIB/code/nr3.h"
@@ -10,16 +11,18 @@ int main()
 {
 	cout << "Find expressions for elements\n";
 	cout << "Read files D1, D2, insert in A\n";
-	int N = 500;
+	ifstream data("../d1");
+	int N;
+	N = std::count(std::istreambuf_iterator<char>(data), std::istreambuf_iterator<char>(), '\n');
 
 	VecDoub theta_1(N);
 	VecDoub theta_2(N);
 	VecDoub x_data(N);
 	VecDoub y_data(N);
-	//layout: theta_1^{(1)}    theta_2^{(1)}   x^{(1)}    y^{(1)}
-	ifstream data("../d1");
+
 	if(data.is_open()){
-		for(int i = 0; i < 500; i++) {
+		data.seekg(0,data.beg);
+		for(int i = 0; i < N; i++) {
 			data >> theta_1[i];
 			data >> theta_2[i];
 			data >> x_data[i];
@@ -29,32 +32,31 @@ int main()
 		cout << "File doesn't exist\n";
 	}
 	cout << "Estimate parameters\n";
+	// calculate the stuff
+//	int N = theta_1.length();
+	MatDoub A(N,3);
 
-    // calculate the stuff
-    int N = theta_1.length();
-    MatDoub A(N,3);
+	cout << "Number of measurements: " << N << endl;
 
-    cout << "Number of measurements: " << N << endl;
+	// computing a, [[1 cos(theta1) cos(theta1 + theta2)],...]
+	for(int i = 0; i < N; i++){
+		A[i][1] = 1;
+		A[i][2] = cos(theta_1[i]);
+		A[i][3] = cos(theta_1[i] + theta_2[i]);
+	}
 
-    // computing a, [[1 cos(theta1) cos(theta1 + theta2)],...]
-    for(int i = 0; i < N; i++){
-        A[i][1] = 1;
-        A[i][2] = cos(theta_1[i]);
-        A[i][3] = cos(theta_1[i] + theta_2[i]);
-    }
+	// creating two sets of equations, A x_1 = b_1 and A x_2 = b_2
+	// where A:[N 3], x_1 and x_2:[3 1] and b_1 and b_2:[N 1]
+	SVD result1(A);
+	SVD result2(A);
 
-    // creating two sets of equations, A x_1 = b_1 and A x_2 = b_2
-    // where A:[N 3], x_1 and x_2:[3 1] and b_1 and b_2:[N 1]
-    SVD result1(A);
-    SVD result2(A);
-
-    result1.u.print();
-    result1.w.print();
-    result1.v.print();
-
-    result2.u.print();
-    result2.w.print();
-    result2.v.print();
+//	result1.u.print();
+//	result1.w.print();
+//	result1.v.print();
+//	cout << "\n\n\n";
+//	result2.u.print();
+//	result2.w.print();
+//	result2.v.print();
 
 	cout << "Estimate resulting errors\n";
 
