3a7468ae898910416976e42e5fa80c8b096d67a0
niive12 <niive12@student.sdu.dk>
2015-03-02 16:34:23 +0000

    it is now a function and it works

--- Code/KNN/01/pca_test.R
+++ Code/KNN/01/pca_test.R
@@ -1,49 +1,44 @@
 source("load_people_data.R")
 
-data = prepareAllMixed(360,40)
 
-N = dim(data$trainSet)[2]
+pca_func_dims <- function(data, breakpoint, k) {
+	data.pca = prcomp(data$trainSet, center=TRUE, scale=FALSE)
 
-data.pca = prcomp(data$trainSet, center=TRUE, scale=FALSE)
-
-sdev_sum_sum = cumsum(data.pca$sdev^2 / sum(data.pca$sdev^2))
-breakpoint = 0.86
-NPC = length(sdev_sum_sum);
-for(i in 1:length(sdev_sum_sum) ){
-	if ( sdev_sum_sum[i] >= breakpoint ) {
-		NPC <- i
-		break
+	sdev_sum_sum = cumsum(data.pca$sdev^2 / sum(data.pca$sdev^2))
+	
+	NPC = length(sdev_sum_sum);
+	for(i in 1:length(sdev_sum_sum) ){
+		if ( sdev_sum_sum[i] >= breakpoint ) {
+			NPC <- i
+			break
+		}
 	}
+	if(NPC == 1){ #this is weird... I will fix later
+		NPC = 2
+	}
+
+	train_data = data.pca$x[,1:NPC]
+	
+
+	test_data = ((data$testSet - data.pca$center) %*% data.pca$rotation)[,1:NPC]
+
+	res = knn(train_data, test_data, data$trainVali, k = k)
+
+	# count right detections
+
+	confus = array(0,c(10,10))
+
+	per <- 0
+	for(i in 1:length(data$testVali)){
+		confus[[data$testVali[i],res[i]]] = confus[data$testVali[i],res[i]] + 1;
+		if(res[i] == (data$testVali)[i]){
+			per <- per + 1
+		} 
+	}
+	per = per/length(data$testVali)
+
+	return(list(confusion_matrix = confus, success = per))
 }
-if(NPC == 1){
-	NPC = 2
-}
-print(paste(c("Number of dimensions for one letter: ",NPC),collapse=""))
 
-
-
-train_data = data.pca$x[,1:NPC]
-
-test_data = ((data$testSet - data.pca$center) %*% data.pca$rotation)[,1:NPC]
-
-
-print(c(dim(train_data),dim(test_data)))
-res = knn(train_data, test_data, data$trainVali, k = 10)
-
-# count right detections
-
-confus = array(0,c(10,10))
-
-per <- 0
-for(i in 1:length(data$testVali)){
-	confus[[data$testVali[i],res[i]]] = confus[data$testVali[i],res[i]] + 1;
-	if(res[i] == (data$testVali)[i]){
-		per <- per + 1
-	} 
-}
-
-print(confus) 
-
-# print(per) #40
-# print(length(data$testVali)) #400
-print(per/length(data$testVali)) #0.1
+data = prepareAllMixed(360,40)
+print(pca_func_dims(data,0.8,10))
