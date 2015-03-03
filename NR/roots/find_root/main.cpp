0882513464f2e6c76485794c58e8b6a2780097a2
niive12 <niive12@student.sdu.dk>
2015-03-03 11:09:23 +0000

    added the func

--- NR/roots/find_root/main.cpp
+++ NR/roots/find_root/main.cpp
@@ -10,32 +10,60 @@
 using namespace std;
 
 double function(double x){
-    return (x-cos(x));
+	return (x-cos(x));
+}
+
+template <class T>
+double my_rtsec(T &func, const Doub x1, const Doub x2, const Doub xacc, Doub order = 1.62) {
+	const Int MAXIT=30;
+	Doub xl,rts;
+	Doub fl=func(x1);
+	Doub f=func(x2);
+	if (abs(fl) < abs(f)) {
+		rts=x1;
+		xl=x2;
+		SWAP(fl,f);
+	} else {
+		xl=x1;
+		rts=x2;
+	}
+	for (Int j=0;j<MAXIT;j++) {
+		Doub dx=(xl-rts)*f/(f-fl);
+		xl=rts;
+		fl=f;
+		double pdk = (rts-(rts-dx));
+		rts += dx;
+		f=func(rts);
+		double dk = (rts-(rts-dx));
+		cout << "K: " << j << "\tx^k: " << setw(10)<< rts << "\td^k: " << setw(10)<< dk << "  diff: " << setw(10)<< fabs(dk)/pow(fabs(pdk),order) << endl;
+		if (abs(dx) < xacc || f == 0.0) return rts;
+	}
+	throw("Maximum number of iterations exceeded in rtsec");
 }
 
 int main() {
 
-    double x1 = 0.0, x2 = PI/2, accuracy = 0.00000001;
+	double x1 = 0.0, x2 = PI/2, accuracy = 0.00000001;
 
-    // bracketing
-    VecDoub b1(0), b2(0);
-    int roots;
-    zbrak(function, x1, x2,100, b1, b2, roots);
+	// bracketing
+	VecDoub b1(0), b2(0);
+	int roots;
+	zbrak(function, x1, x2,100, b1, b2, roots);
 
-    for(int i = 0; i < roots; i++){
-        cout << "Result within: " << b1[i] << " : " << b2[i] << endl;
-    }
+	for(int i = 0; i < roots; i++){
+		cout << "Result within: " << b1[i] << " : " << b2[i] << endl;
+	}
 
-    // bisektion
-    Doub root = rtbis(function, x1, x2, accuracy);
+	// bisektion
+	Doub root = rtbis(function, x1, x2, accuracy);
 
-    cout << root << endl;
+	cout << root << endl;
 
-    // sekant
-    root = rtsec(function, x1, x2, accuracy);
+	// sekant
+	root = my_rtsec(function, x1, x2, accuracy);
 
-    cout << root << endl;
+	cout << root << endl;
 
 
-    return 0;
+	return 0;
 }
