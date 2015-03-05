84137836d49b45a7676c451bc0dc96835a1c94f1
Croomch <lschw12@student.sdu.dk>
2015-03-05 14:22:04 +0100

    test file..

--- Code/KNN/01/normalization_test.R
+++ Code/KNN/01/normalization_test.R
@@ -1,46 +1,63 @@
+library("graphics")
+
 source("load_people_data.R")
 source("pca_test.R")
 source("normalize.R")
 
+
 # compare one from dataset to all others (do with 10 different ppl)
 # plot sucess for that person
 # do with normalization before and after with both normalizations
+# best plot: k = 19, PCA = 0.8
+# prepareOneAlone(g,m, trainPartSize = 100, testSize = 40, peopleToLoad = getPeople())
 
 # take plot way from: results.R
 # b = streg imellem, o = streg igennem
 
-testPerson <- c(3,2)
-PCA = 0.9
-k = 1
 
-runs = 10;
-split = 0.9
-
-print("1 person at 100DPI..")
-# make data
-prepareAllMixedCrossVal(split,runs,peopleToLoad = list(testPerson))
-
-result <- matrix(0,4,runs)
+PCA = 0.8
+k = 19
 
 
-# finalData <- list(trainSet=train,testSet=test,trainVali=train_actual,testVali=test_actual)
-for(runType in 1:2){
-	for(run in 1:runs){
-		load("crossVal_DPI100_",split,"_FILTERnone_",run,".RData")
-		
-		preNormalized <- normalizeData(finalData, "min-max")
-		preNormalized <- pca_simplification(preNormalized, breakpoint = PCA)
-		
-		postNormalized <- pca_simplification(finalData, breakpoint = PCA)
-		postNormalized <- normalizeData(postNormalized, "min-max")
-		
-		result[((runtype-1)*2 + 1),runs] <- run_knn(preNormalized,k) # pre
-		result[((runtype-1)*2 + 2),runs] <- run_knn(postNormalized,k) # post
-	}
+
+# prep for data
+people <- getPeople()
+noPeople <-length(people)
+result <- matrix(0,4,noPeople)
+
+
+# make loop to save data
+for(person in 1:noPeople){
+	data <- prepareOneAlone(people[[person]][1],people[[person]][2], trainPartSize = 100, testSize = 40, peopleToLoad = people)
+	
+	# min max
+	preNormalized <- normalizeData(data, "min-max")
+	preNormalized <- pca_simplification(preNormalized, breakpoint = PCA)
+	
+	postNormalized <- pca_simplification(data, breakpoint = PCA)
+	postNormalized <- normalizeData(postNormalized, "min-max")
+	
+	result[1,person] <- run_knn(preNormalized,k)$success # pre
+	result[2,person] <- run_knn(postNormalized,k)$success # post
+	
+	# z-score
+	preNormalized <- normalizeData(data, "z-score")
+	preNormalized <- pca_simplification(preNormalized, breakpoint = PCA)
+	
+	postNormalized <- pca_simplification(data, breakpoint = PCA)
+	postNormalized <- normalizeData(postNormalized, "z-score")
+	
+	result[3,person] <- run_knn(preNormalized,k)$success # pre
+	result[4,person] <- run_knn(postNormalized,k)$success # post
+	
+	print(paste(c(person, "/", noPeople, " datasets loaded."),collapse = ""))
 }
+
+
 
 print(result)
 
+# mean
 mean <- 1:4
 
 for(i in 1:4){
@@ -49,11 +66,36 @@ for(i in 1:4){
 
 print("mean")
 print(mean)
+#  0.43975 0.34050 0.46850 0.40775
 
-# setEPS()
-# postscript("success_vs_dpi.eps",height = 4, width = 8)
-# plot(dpi,time,type="b",xlab="DPI",ylab="time [s]",ylim=(c(250,1600)),col="black")
-# dev.off()
 
+#result
+
+#make x string
+x_lab <- 1:noPeople
+for(i in 1:noPeople){
+	x_lab[i] <- paste(c(people[[i]][1],":",people[[i]][2]),collapse="")
+}
+
+print(x_lab)
+
+colors = rainbow(4)
+plot(1:noPeople,result[1,], xaxt="n",type="b",xlab="Person",ylab="Success Rate",ylim=(c(min(result),max(result))),col=colors[1]) # ylim=(c(min(result),max(result))),
+axis(1, at=1:noPeople, labels=x_lab)
+# legend("topright",c("min-max pre","min-max post","z-score pre","z-score post"), fill=colors, horiz=TRUE)
+legend("bottomright",c("min-max pre","min-max post","z-score pre","z-score post"),cex=0.8,col=colors,lty=1:4)
+for(i in 2:4){
+	lines(1:noPeople,result[i,],type="b",lty=i, col=colors[i])
+}
+setEPS()
+postscript("graph.eps",height = 6, width = 8)
+plot(1:noPeople,result[1,], xaxt="n",type="b",xlab="Person",ylab="Success Rate",ylim=(c(min(result),max(result))),col=colors[1]) # ylim=(c(min(result),max(result))),
+axis(1, at=1:noPeople, labels=x_lab)
+# legend("topright",c("min-max pre","min-max post","z-score pre","z-score post"), fill=colors, horiz=TRUE)
+legend("bottomright",c("min-max pre","min-max post","z-score pre","z-score post"),cex=0.8,col=colors,lty=1:4)
+for(i in 2:4){
+	lines(1:noPeople,result[i,],type="b",lty=i, col=colors[i])
+}
+dev.off()
 
 
