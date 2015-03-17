04b0d400454d4371d70f63ca3477b114020824e0
Croomch <lschw12@student.sdu.dk>
2015-03-17 10:11:30 +0100

    task and function added

---
+++ NR/rectangle/rectangle/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include "../NR_LIB/code/nr3.h"
+
+using namespace std;
+
+void function(float x){
+    // cos(x^2)*e^(-x)/sprt(x)
+    float result = cos(pow(2,x))*exp(-x)/sqrt(x);
+    return result;
+}
+
+
+// integrate(cos(x^2)*e^(-x)/sprt(x) dx) from 0 to 1 using rectangle
+// show iterations, S(h_k), Rich-order estimate, Rich-error estimate
+
+int main() {
+
+
+    return 0;
+}
---
+++ NR/rectangle/rectangle/rectangle.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-03-17T10:02:33
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = rectangle
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
