047c6640da9883dd08e28d3f829450c6bd738b09
Croomch <lschw12@student.sdu.dk>
2015-03-24 10:54:29 +0100

    NR function updated

--- NR/integrate/integrate/main.cpp
+++ NR/integrate/integrate/main.cpp
@@ -3,18 +3,19 @@
 #include "../../NR_LIB/code/nr3.h"
 #include "../../NR_LIB/code/derule.h"
 
-double function(double x){
+double function(double x, double tau){
+    double result;
 //   cos(pow(x,2))*exp(-x)
 //   cos(pow(x,2))*exp(-x)/sqrt(x) // hint: sqrt(tau)...
 
-//    double result = (cos(pow(x,2))*exp(-x));
-    double result = (cos(pow(x,2))*exp(-x))/sqrt(x);
+    result = (cos(pow(x,2))*exp(-x));
+//    result = (cos(pow(x,2))*exp(-x))/sqrt(tau);
     return result;
 }
 
 
 
-// integrate using DErule
+// integrate using DErule integrate from 0 to 1
 // show result for different N's
 
 int main() {
