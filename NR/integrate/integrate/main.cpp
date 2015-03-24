6756c1a7d81798809558e56e6383bca1c59f20b4
niive12 <niive12@student.sdu.dk>
2015-03-24 11:17:38 +0100

    updated print

--- NR/integrate/integrate/main.cpp
+++ NR/integrate/integrate/main.cpp
@@ -21,26 +21,25 @@ double function(double x, double tau){
 // integrate using DErule integrate from 0 to 1
 // show result for different N's
 template <class T>
-void print_DErule(T &funcc, const Doub aa, const Doub bb, const Doub hmaxx=3.7){
-
+void print_DErule(T &funcc, const Doub aa, const Doub bb, int max_iter=100, const Doub hmaxx=3.7){
+	int width = 17;
+	cout << setw(4) << "iter"<< "\t";
+	cout << setw(width) << "s_h3" << "\t";
+	cout << setw(width) << "s_h3-s_h2" << "\n";
+	cout.precision(15);
 	double s_h1 = 1, s_h2 = 1, s_h3 = 1, alpha_k, error;
 	DErule<T> int_derule(funcc, aa, bb, hmaxx);
-	int width = 10;
-	for(ever){
+	for(int iterations = 0; iterations < max_iter; iterations++){
 		s_h1 = s_h2;
 		s_h2 = s_h3;
 		s_h3 = int_derule.next();
 		alpha_k = (s_h1-s_h2)/(s_h2-s_h3);
 		error = (s_h2-s_h1)/(alpha_k-1);
 
-//		if (iterations > 2 ) {
-//			cout << setw(4) << iterations << "\t";
+			cout << setw(4) << iterations << "\t";
 			cout << setw(width) << s_h3 << "\t";
-			cout << setw(width) << alpha_k << "\t";
-			cout << setw(width) << log(alpha_k)/log(3) << "\t";
-			cout << setw(width) << error << "\t";
+			cout << setw(width) << s_h3-s_h2 << "\t";
 			cout << endl;
-//		}
 	}
 }
 
