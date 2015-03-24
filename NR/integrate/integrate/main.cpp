0ff431a70d26884ae29bb0ae22452f32c4cda799
Croomch <lschw12@student.sdu.dk>
2015-03-24 11:12:03 +0100

    Function update for NR

--- NR/integrate/integrate/main.cpp
+++ NR/integrate/integrate/main.cpp
@@ -6,13 +6,20 @@
 #define ever ;;
 
 double function(double x, double tau){
-	double result;
-//   cos(pow(x,2))*exp(-x)
-//   cos(pow(x,2))*exp(-x)/sqrt(x) // hint: sqrt(tau)...
+    double result;
+//    cos(pow(x,2))*exp(-x) = 0.590051 from 0 to 1
+//    cos(pow(x,2))*exp(-x)/sqrt(x) = 1.44599 from 0 to 1 // hint: sqrt(tau)...
 
-	result = (cos(pow(x,2))*exp(-x));
-//    result = (cos(pow(x,2))*exp(-x))/sqrt(tau);
-	return result;
+    result = (cos(pow(x,2))*exp(-x));
+
+//    if(x < 0.00001){
+//        result = (cos(pow(x,2))*exp(-x))/sqrt(tau);
+//    }
+//    else{
+//        result = (cos(pow(x,2))*exp(-x))/sqrt(x);
+//    }
+
+    return result;
 }
 
 
@@ -22,29 +29,29 @@ double function(double x, double tau){
 template <class T>
 void print_DErule(T &funcc, const Doub aa, const Doub bb, const Doub hmaxx=3.7){
 
-	double s_h1 = 1, s_h2 = 1, s_h3 = 1, alpha_k, error;
-	DErule<T> int_derule(funcc, aa, bb, hmaxx);
+    double s_h1 = 1, s_h2 = 1, s_h3 = 1, alpha_k, error;
+    DErule<T> int_derule(funcc, aa, bb, hmaxx);
 
-	for(ever){
-		s_h1 = s_h2;
-		s_h2 = s_h3;
-		sh_3 = int_derule.next();
-		alpha_k = (s_h1-s_h2)/(s_h2-s_h3);
-		error = (s_h2-s_h1)/(alpha_k-1);
+    for(ever){
+        s_h1 = s_h2;
+        s_h2 = s_h3;
+        sh_3 = int_derule.next();
+        alpha_k = (s_h1-s_h2)/(s_h2-s_h3);
+        error = (s_h2-s_h1)/(alpha_k-1);
 
-//		if (iterations > 2 ) {
-			cout << setw(4) << iterations << "\t";
-			cout << setw(width) << s_h3 << "\t";
-			cout << setw(width) << alpha_k << "\t";
-			cout << setw(width) << log(alpha_k)/log(3) << "\t";
-			cout << setw(width) << error << "\t";
-			cout << endl;
-//		}
-	}
+        //		if (iterations > 2 ) {
+        cout << setw(4) << iterations << "\t";
+        cout << setw(width) << s_h3 << "\t";
+        cout << setw(width) << alpha_k << "\t";
+        cout << setw(width) << log(alpha_k)/log(3) << "\t";
+        cout << setw(width) << error << "\t";
+        cout << endl;
+        //		}
+    }
 }
 
 int main() {
 
 
-	return 0;
+    return 0;
 }
