e5aca69a166b686b7bf7da7ca97b9e98ed9327e7
niive12 <niive12@student.sdu.dk>
2015-03-24 10:59:29 +0100

    added print

--- NR/integrate/integrate/main.cpp
+++ NR/integrate/integrate/main.cpp
@@ -3,22 +3,47 @@
 #include "../../NR_LIB/code/nr3.h"
 #include "../../NR_LIB/code/derule.h"
 
+#define ever ;;
+
 double function(double x){
 //   cos(pow(x,2))*exp(-x)
 //   cos(pow(x,2))*exp(-x)/sqrt(x) // hint: sqrt(tau)...
 
 //    double result = (cos(pow(x,2))*exp(-x));
-    double result = (cos(pow(x,2))*exp(-x))/sqrt(x);
-    return result;
+	double result = (cos(pow(x,2))*exp(-x))/sqrt(x);
+	return result;
 }
 
 
 
 // integrate using DErule
 // show result for different N's
+template <class T>
+void print_DErule(T &funcc, const Doub aa, const Doub bb, const Doub hmaxx=3.7){
+
+	double s_h1 = 1, s_h2 = 1, s_h3 = 1, alpha_k, error;
+	DErule<T> int_derule(funcc, aa, bb, hmaxx);
+
+	for(ever){
+		s_h1 = s_h2;
+		s_h2 = s_h3;
+		sh_3 = int_derule.next();
+		alpha_k = (s_h1-s_h2)/(s_h2-s_h3);
+		error = (s_h2-s_h1)/(alpha_k-1);
+
+//		if (iterations > 2 ) {
+			cout << setw(4) << iterations << "\t";
+			cout << setw(width) << s_h3 << "\t";
+			cout << setw(width) << alpha_k << "\t";
+			cout << setw(width) << log(alpha_k)/log(3) << "\t";
+			cout << setw(width) << error << "\t";
+			cout << endl;
+//		}
+	}
+}
 
 int main() {
 
 
-    return 0;
+	return 0;
 }
