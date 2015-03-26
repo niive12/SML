36a7684bafdd5d32f5f638b502e42ec227a0e63e
niive12 <niive12@student.sdu.dk>
2015-03-26 13:44:25 +0100

    kde

--- Code/KNN/01/kde_test.R
+++ Code/KNN/01/kde_test.R
@@ -39,7 +39,26 @@ estimated_pdf = list(c(1:10,1:10))
 for( i in 1:10) {
 	estimated_pdf[[i]] = bkde(x=new_trainset[,i], kernel="normal")
 }
-plot(estimated_pdf[[1]], xlab="x", ylab="pdf")
-for( i in 2:10) {
-	lines(estimated_pdf[[i]])#, xlab="x", ylab="pdf")
+
+xmas = 1:10
+xmis = 1:10
+ymas = 1:10
+ymis = 1:10
+for(i in 1:10){
+	xmas[i] = max(estimated_pdf[[i]]$x)
+	xmis[i] = min(estimated_pdf[[i]]$x)
+	ymas[i] = max(estimated_pdf[[i]]$y)
+	ymis[i] = min(estimated_pdf[[i]]$y)
 }
+
+xmax = max(xmas)
+xmin = min(xmis)
+ymax = max(ymas)
+ymin = min(ymis)
+colors = rainbow(10)
+
+plot(estimated_pdf[[1]], xlab="x", ylab="pdf", xlim=c(xmin,xmax), ylim=c(ymin,ymax),type="l",col=colors[1],lty=1)
+for( i in 2:10) {
+	lines(estimated_pdf[[i]],col=colors[i],lty=i)#, xlab="x", ylab="pdf")
+}
+legend("topright",NULL,0:9,cex=0.8,col=colors,lty=1:3)
