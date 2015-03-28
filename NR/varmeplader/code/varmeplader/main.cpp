a10e7ec1a7b2e2236aea06bd0430177bdc78016d
niive12 <niive12@student.sdu.dk>
2015-03-28 10:52:58 +0100

    code

--- NR/varmeplader/code/varmeplader/main.cpp
+++ NR/varmeplader/code/varmeplader/main.cpp
@@ -2,52 +2,70 @@
 #include <cmath>
 #include "../../../NR_LIB/code/nr3.h"
 #include "../../../NR_LIB/code/quadrature.h"
-//#include "../../../NR_LIB/code/derule.h"
 
-double func_F(double x, double y, double d){
+double func_F_x(double x){
+	const double d = 1.0;
+	double y = 0;
 	return 0.5* (pow(d,2)/pow((pow(d,2)+pow(x-y,2)),3/2));
 }
 
-double int1_func(double x, double aa, double bb){
-
+double func_F_y(double y){
+	const double d = 1.0;
+	double x = 0;
+	return 0.5* (pow(d,2)/pow((pow(d,2)+pow(x-y,2)),3/2));
 }
 
-double int2_func(double y, double aa, double bb){
+template<class T>
+double int_func(T &funcc, double aa, double bb){
+	const double min_error = 10e-6;
+	int max_iter = 20;
+	double s_h1 = 1, s_h2 = 1, s_h3 = 1, error;
 
+	Trapzd<T> int_trapez(funcc, aa, bb);
+	for(int iterations = 0; iterations < max_iter; iterations++){
+		s_h1 = s_h2;
+		s_h2 = s_h3;
+		s_h3 = int_trapez.next();
+		error = (s_h1-s_h2)/(s_h2-s_h3);
+		if(error < min_error){
+			return s_h3;
+		}
+	}
+	throw("Too many iterations in int_func");
+	return min_error;
 }
 
-
-double func_u(double x){
+double func_u(double v_0){
 	int T=1000;
 	double eps = 0.80;
 	double sigma = 1.712*10e-9;
-	double d = 1.0, w=1.0;
+	double w=1.0;
 
-	double I = int1_func(x, -0.5*w, 0.5*w);
-	eps*sigma*pow(T,4)+(1-eps);
-
+	double I = int_func(func_F_x,-0.5*w, 0.5*w) * v_0;
+	return eps*sigma*pow(T,4)+(1-eps)*I;
 }
 
-double func_v(double y){
+double func_v(double u_0){
 	int T=500;
 	double eps = 0.60;
 	double sigma = 1.712*10e-9;
-	double d = 1.0, w=1.0;
+	double w=1.0;
 
-	double I = int2_func(y, -0.5*w, 0.5*w);
-	eps*sigma*pow(T,4)+(1-eps)*I;
-}
-
-
-
-// integrate using DErule integrate from 0 to 1
-// show result for different N's
-template <class T>
-void print_task(T &funcc, const Doub aa, const Doub bb, int max_iter=100, const Doub hmaxx=3.7){
-
+	double I = int_func(func_F_x,-0.5*w, 0.5*w) * u_0;
+	return eps*sigma*pow(T,4)+(1-eps)*I;
 }
 
 int main() {
-	print_task(function,0,1);
+	double u_0=0,v_0=0,u_1,v_1;
+	int N = 16;
+
+	for( int i = 0; i < N; i++){
+		u_1 = func_u(v_0);
+		v_1 = func_v(u_0);
+		u_0 = u_1;
+		v_0 = v_1;
+		cout << "u = " << u_0 << "\tv = " << v_0 << endl;
+	}
+
 	return 0;
 }
