6e54a33b366f83f8aaa8ef81afea4aed42c2c9fa
niive12 <niive12@student.sdu.dk>
2015-04-07 10:14:20 +0200

    some cleanup

--- NR/ODE/main.cpp
+++ NR/ODE/main.cpp
@@ -4,14 +4,17 @@
 using namespace std;
 
 template <class T>
-double euler_method(T &func, double x_0, double alpha, double h){
+double euler_method(T &func, double alpha, double h, double x_start, int N){
 	static double y_n0;
+	static int n = 0;
 	double y_n1;
-	n = 0;
+	double x_0;
+	x_0 = x_start+n*h;
 	if(n == 0){
 		y_n0 = alpha;
 	}
 	y_n1 = y_n0 + h*func(y_n0,x_0);
+	n++;
 	return y_n1;
 }
 
