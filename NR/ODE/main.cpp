f76ed93d88301f2c6288cc04fe65064b25a9f9b7
niive12 <niive12@student.sdu.dk>
2015-04-07 10:03:43 +0200

    inital thoughts

--- NR/ODE/main.cpp
+++ NR/ODE/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <../NR_LIB/code/nr3.h>
 
 using namespace std;
 
+template <class T>
+double euler_method(T &func, double x_0, double alpha, double h){
+	static double y_n0;
+	double y_n1;
+	n = 0;
+	if(n == 0){
+		y_n0 = alpha;
+	}
+	y_n1 = y_n0 + h*func(y_n0,x_0);
+	return y_n1;
+}
+
 int main()
 {
-	cout << "Hello World!" << endl;
 	return 0;
 }
 
