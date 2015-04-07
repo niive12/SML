68707dec110e44aeb6fd0ce1cb54c69f65541c68
Croomch <lschw12@student.sdu.dk>
2015-04-07 10:49:57 +0200

    NR: stuff made¨

--- NR/ODE/main.cpp
+++ NR/ODE/main.cpp
@@ -4,25 +4,26 @@
 
 using namespace std;
 
-vector<double> y_marks(vector<double> prev){
+double y_marks(vector<double> prev,int i){
     vector<double> res(2);
 
     res[0] = prev[0]*prev[1];
     res[1] = -pow(prev[0],2);
 
-    return res;
+    return res[i];
 }
 
 
 template <class T>
-vector<double> euler_method(vector<T> &func, vector<double> init, double N, double h){
+vector<double> euler_method(T &func, vector<double> init, int N, double h){
 
     vector<double> x_vals_prev = init;
     vector<double> x_vals_new = init;
 
     for(int n = 0; n < N; n++){
-        for(int i = 0; i < func.size(); i++){
-            x_vals_new = x_vals_prev + h*(func[i](x_vals_prev));
+        for(int i = 0; i < 2; i++){
+            double mark = func(x_vals_prev,i);
+            x_vals_new[i] = x_vals_prev[i] + h*(mark);
         }
         x_vals_prev = x_vals_new;
     }
@@ -41,8 +42,9 @@ int main()
     initials[1] = 1;
 
     vector<double> result(2);
-
-    result = euler_method(y_marks, initials, 20, 0.1);
+    int N = 1;
+    double h = 1;
+    result = euler_method(y_marks, initials, N, h);
 
     for(int i = 0; i < result.size();  i++){
         cout << result[i] << endl;
