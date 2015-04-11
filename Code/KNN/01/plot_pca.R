18a925a668b3377418d7cad1f8f4d65f8eded72d
niive12 <niive12@student.sdu.dk>
2015-04-11 12:17:45 +0200

    plot function done

--- Code/KNN/01/plot_pca.R
+++ Code/KNN/01/plot_pca.R
@@ -10,16 +10,14 @@ length = size;
 
 x_axis = matrix(0,size,10)
 
-
+digit = 3
 
 for( i in 1:size ){
 	for(d in 0:9){
 		x_axis[i,d+1] = data$trainSet[i+d*length,][1]
 	}
 }
-print(dim(data$trainSet))
-div = entropy(component=data$trainSet[,1], classifications=data$trainVali, divisions = 200)
-print(div)
+div = entropy(component=data$trainSet[,digit], classifications=data$trainVali, divisions = 200)
 
 xlabel = seq(round(min(x_axis)-0.05,digits=1),max(x_axis)+0.1,0.1) 
 
@@ -28,21 +26,12 @@ setEPS()
 postscript("Rplots.pdf",height = 4, width = 8)
 for(i in 1:10){
 	if(i == 1){
-		plot(x_axis[,i], array(i,size), pch=4, col="black", xlim=c(min(xlabel),max(xlabel)), ylim=c(0,10), axes=FALSE, xlab="", ylab="")
+		plot(x_axis[,i], array(i,size), pch=4, col=colors[i], xlim=c(min(xlabel),max(xlabel)+0.6), ylim=c(0,10), axes=FALSE, xlab="", ylab="")
 	} else {
 		lines(x_axis[,i], array(i,size), type="p", pch=4, col=colors[i])
 	}
 }
-segments(div$divider, 0 , div$divider, 20, col="red")
+segments(div$divider, 0 , div$divider, 20, col="black")
 axis(1, at=xlabel)
+legend("topright",NULL,9:0,cex=0.95,col=rev(colors),lty=array(1,10))
 q = dev.off()
-
-pc1 = matrix(0,size,10)
-for(i in 1:size ) {
-	for( d in 0:9 ){
-		pc1[i,d+1] = data$trainSet[i+d*length,][1]
-	}
-}
-for(i in 1:10 ){
-	print(c(i-1,mean(pc1[,i]),var(pc1[,i])))
-}
