fca49720481a822c42ef0b7b22996d9d8147c73d
niive12 <niive12@student.sdu.dk>
2015-04-28 03:49:49 +0200

    included libraries

--- NR/Shoot/main.cpp
+++ NR/Shoot/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <stdlib.h>
 #include "../NR_LIB/code/nr3.h"
 #include "../NR_LIB/code/odeint.h"
+#include "../NR_LIB/code/stepper.h"
+#include "../NR_LIB/code/stepperdopr853.h"
 #include "../NR_LIB/code/shoot.h"
-#include "../NR_LIB/code/sphoot.h"
+
+#include "../NR_LIB/code/stepperdopr5.h"
 
 
-int main()
+int main(int argc, char** argv)
 {
+	//u(x) for 0<=x<=1
+	//u''(x) = sqrt(1+pow(u'(x),2))
 
-    return true;
+	//u(0) = 0 , u(1) = 1
+
+	return EXIT_SUCCESS;
 }
